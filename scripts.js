0643dd876fda7196798d8b92038ca11783c3472e
abhipassi <abhijit36254@gmail.com>
2024-08-08 22:47:07 +0530

    Function practice

--- scripts.js
+++ scripts.js
@@ -1,23 +1,78 @@
-// Functions in Javascript
+// // Functions in Javascript
 
-// function myFunction(){
-//     console.log("Hello");
-//     console.log("My name is Abhijit");
+// // function myFunction(){
+// //     console.log("Hello");
+// //     console.log("My name is Abhijit");
+// // }
+// // myFunction();
+
+// // function for sum of two numbers
+// // function sum(x,y){
+// //     console.log(x+y);
+// // }
+// // sum(3,4);
+
+// // Sum Function
+// function Sum(a,b){
+//     return a+b;
 // }
-// myFunction(); 
-
-// function for sum of two numbers
-// function sum(x,y){
-//     console.log(x+y);
+// // Multiplication
+// function mul(a,b){
+//     return
 // }
-// sum(3,4);
 
+// sum function
+// function sum(a,b){
+//     return a+b;
+// }
+// let x = sum(10,3434);
+// console.log(x);
 
-// Sum Function
-function Sum(a,b){
-    return a+b;
-}
-// Multiplication
-function mul(a,b){
-    return
+// function sum1(a,b){
+//     console.log(a+b);
+
+// }
+// sum1(3,4);
+// Arrow functions
+
+// let sum = (a,b) => {
+//     console.log(a+b);
+// }
+// sum(200,200);
+
+// let mul =(a,b)=>{
+//     return a*b;
+// }
+// mul(2,4);
+
+// practice questions
+
+// Qs. Create a function using the “function” keyword that takes a String as an argument &
+// returns the number of vowels in the string.
+// function countVowels(str) {
+//   let count = 0;
+//   for (char of str) {
+//     if (
+//       char == "a" ||
+//       char == "e" ||
+//       char == "i" ||
+//       char == "o" ||
+//       char == "u"
+//     ) {
+//       count++;
+//     }
+//   }
+//    return count;
+// }
+
+let countVow = (str) => {
+    let count = 0;
+    for (char of str){
+        if(
+            char == "a" || char == "e" || char == "i" || char == "o" || char == "u"
+        ){
+            count++
+        }
+    }
+    return count;
 }
