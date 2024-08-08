b2e01bcc6b1afc213037ed1e495a6ecf34f38c32
abhipassi <abhijit36254@gmail.com>
2024-08-08 19:16:26 +0530

    Added new files

---
+++ scripts.js
@@ -0,0 +1,23 @@
+// Functions in Javascript
+
+// function myFunction(){
+//     console.log("Hello");
+//     console.log("My name is Abhijit");
+// }
+// myFunction(); 
+
+// function for sum of two numbers
+// function sum(x,y){
+//     console.log(x+y);
+// }
+// sum(3,4);
+
+
+// Sum Function
+function Sum(a,b){
+    return a+b;
+}
+// Multiplication
+function mul(a,b){
+    return
+}
