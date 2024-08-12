fb86351c103580feadc902d05f29f0756fc762fd
abhipassi <abhijit36254@gmail.com>
2024-08-12 20:17:56 +0530

    Arrow function

--- scripts.js
+++ scripts.js
@@ -64,15 +64,15 @@
 //   }
 //    return count;
 // }
-
-let countVow = (str) => {
-    let count = 0;
-    for (char of str){
-        if(
-            char == "a" || char == "e" || char == "i" || char == "o" || char == "u"
-        ){
-            count++
-        }
-    }
-    return count;
-}
+// using arrow function
+// let countVow = (str) => {
+//     let count = 0;
+//     for (char of str){
+//         if(
+//             char == "a" || char == "e" || char == "i" || char == "o" || char == "u"
+//         ){
+//             count++
+//         }
+//     }
+//     return count;
+// }
