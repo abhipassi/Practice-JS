9e12a7fd5c291c2031fce567bdfe4545fbe8afe2
abhipassi <abhijit36254@gmail.com>
2024-08-15 23:00:47 +0530

    Conversion of datatypes

---
+++ 03conversion.js
@@ -0,0 +1,31 @@
+// let value = "33";
+// console.log(typeof value);
+// let newNumber = Number(value); // using this way we can change the type of data 
+// console.log(newNumber);
+// console.log(typeof newNumber);
+
+// let str = 33;
+// console.log(typeof str); //before conversion
+// let newStr = String(str);
+// console.log(typeof newStr); //after the conversion we get 33 as a string 
+
+// same way we can do with the the other data types
+// "33"  = 33
+// "33abc" = NaN(not a number) because 33abc is not a pure number that is why on conversion it will give NaN
+// true = 1 
+// false = 0
+// number into Boolean
+let loggedIn = 1;
+let newVal = Boolean(loggedIn);
+console.log(typeof loggedIn);
+console.log(typeof newVal);
+console.log(newVal);
+
+
+
+
+
+
+
+
+
