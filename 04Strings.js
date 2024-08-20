2d853a74b8b9a6d36129ca5c7f561ce22d25af82
abhipassi <abhijit36254@gmail.com>
2024-08-20 22:44:24 +0530

    About Strings in js

---
+++ 04Strings.js
@@ -0,0 +1,13 @@
+const myName = "Abhijt_Passi";
+// console.log(myName.charAt(2));
+// console.log(myName.toUpperCase());
+// console.log(myName.toLowerCase());
+const newString = myName.substring(0, 4);
+console.log(newString);
+const anotherString = myName.slice(0 , 4);
+console.log(anotherString);
+
+
+
+
+
