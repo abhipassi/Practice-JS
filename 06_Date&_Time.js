70608350787b19023d2fc7a1ddaaa867b81a284c
abhipassi <abhijit36254@gmail.com>
2024-08-26 15:29:11 +0530

    date and time

---
+++ 06_Date&_Time.js
@@ -0,0 +1,28 @@
+// Date is a object 
+let myDate = new Date();
+// console.log(typeof myDate);
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+
+let customDate = new Date(2004 , 2 , 19); // to declare custom date 
+//console.log(customDate.toDateString()); // to make the date into readable form 
+
+let newDate = new Date();
+console.log(newDate.getDay())
+console.log(newDate.getMonth())
+console.log(newDate.getSeconds())
+console.log(newDate.getMinutes())
+console.log(newDate.getDate())
+
+
+
+
+
+
+
+
