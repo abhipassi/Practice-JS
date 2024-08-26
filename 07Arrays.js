c0fa030030c480e1f05ebdda32a1a953b017d9fe
abhipassi <abhijit36254@gmail.com>
2024-08-26 16:06:53 +0530

    Array methods

---
+++ 07Arrays.js
@@ -0,0 +1,18 @@
+let myArr = [0,1,2,3,4,5,6]
+// array methods
+// myArr.push(7 ,8)
+// myArr.pop()
+// myArr.unshift(9)
+// myArr.shift()
+
+// slice and splice
+let slarr = myArr.slice(1,3)
+console.log("A =",myArr)
+console.log(slarr);
+
+let sparr = myArr.splice(1,3)
+console.log("B =",myArr)
+console.log(sparr);
+
+
+
