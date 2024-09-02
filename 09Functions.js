47a82cdcdd7352f4d329a921a4172f506adc24a8
abhipassi <abhijit36254@gmail.com>
2024-09-02 16:09:59 +0530

    passing of array in function

--- 09Functions.js
+++ 09Functions.js
@@ -15,4 +15,30 @@ function user(userName) {
   }
   console.log(`${userName} just logged in`);
 }
-user("Abhijit");
+// user("Abhijit");
+
+
+function calculateCartPrice(...num1){ //rest operator in this we can give infinte number of value
+  return num1
+}
+// console.log(calculateCartPrice(200, 2224,424,2323,323,2424));
+
+
+//  passing objects in function
+let userDetail = {
+  name: "Abhijit",
+  age: 21,
+  height: 5.11
+}
+function user(Newobj){
+  return `Name of the user is ${Newobj.name} and age of the user is ${Newobj.age} and height of the user is ${Newobj.height}`
+}
+// console.log(user(userDetail));
+
+// passing array in function 
+let price = [200,3000,323,323,45,423,53323435,23353]
+
+function arrayP(arr){
+  return `the price of third item is ${arr[2]}`
+}
+// console.log(arrayP(price));
