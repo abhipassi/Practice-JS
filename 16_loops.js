5f6fc6358f94b4bbad52244c52b2ac4dd7353483
abhipassi <abhijit36254@gmail.com>
2024-09-10 22:52:54 +0530

    for loop

---
+++ 16_loops.js
@@ -0,0 +1,60 @@
+// for loop
+// for (c = 1 ; c <= 10; c++){
+    // console.log(c);
+// }
+// let input = "Abhijit"
+// const str = (input) =>{
+//     let count = 0
+//     for (let char of input){
+//         if(char == "a" || char == "e" || char == "i" || char == "o" || char == "u"){
+//             count ++
+//         } 
+//     }
+//     console.log(count);
+    
+// }
+// let str = "Abhijit"
+// function countVowels(str) {
+//   let count = 0;
+//   for (char of str) {
+//     if (
+//       char == "a" ||
+//       char == "e" ||
+//       char == "i" ||
+//       char == "o" ||
+//       char == "u"
+//     ) {
+//       count++;
+//     }
+//   }
+//    return count;
+// }
+
+
+// for (let i = 1; i <=10; i++){
+//   console.log(`the outer loop ${i}`);
+//   for (let j = 1 ; j <=10; j++){
+//     console.log(`the inner loop ${j} outer loop ${i}`);
+    
+//   }
+// }
+// print tables 
+
+// for (let i = 1; i <= 10; i++){
+//   console.log(`Table of ${i}`);
+//   for (let j = 1; j <= 10; j++){
+//     // console.log(i + '*' + j + '=' + i*j);
+//     let k = i * j
+//     console.log(`${i} * ${j} = ${k}`)
+    
+//   }
+// }
+
+
+for (let i = 1 ; i <=20 ; i++){
+  if(i == 5 || i == 6 || i ==  7){
+    console.log(`5 is Detected`);/
+    continue
+  }
+  console.log(`the value of i is ${i}`);
+}
